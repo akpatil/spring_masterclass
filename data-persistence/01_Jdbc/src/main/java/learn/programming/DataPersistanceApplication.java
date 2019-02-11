f3231a6d61972f7a0b58e2752980c00bdf2dcc3c
akpatil <akshay78661@gmail.com>
2019-02-11 22:12:17 +0530

    Persistence commit

--- data-persistence/01_Jdbc/src/main/java/learn/programming/DataPersistanceApplication.java
+++ data-persistence/01_Jdbc/src/main/java/learn/programming/DataPersistanceApplication.java
@@ -1,11 +1,14 @@
 package learn.programming;
 
 import learn.programming.dao.PersonJdbcDao;
+import learn.programming.entity.Person;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @Slf4j
 @SpringBootApplication
@@ -19,6 +22,14 @@ public class DataPersistanceApplication implements CommandLineRunner {
         log.info("Persons findAll method called: {}", dao.findAll());
         log.info("Persons findById method called: {}", dao.findById(1003));
         log.info("Persons deleteById method called: {}", dao.deleteById(1005));
+        log.info("Persons insert method called {}", dao.insert(
+                new Person(1007, "Claire Redfield", "Racoon City", new Date())));
+        log.info("Persons findAll method called: {}", dao.findAll());
+
+        log.info("Persons update method called {}", dao.update(
+                new Person(1007, "Chris Redfield", "Racoon City", new Date())));
+        log.info("Persons findAll method called: {}", dao.findAll());
+        log.info("Person findAllByCustomMapper method called: {}", dao.findAllByCustomMapper());
     }
 
     public static void main(String[] args) {
--- data-persistence/01_Jdbc/src/main/java/learn/programming/dao/PersonJdbcDao.java
+++ data-persistence/01_Jdbc/src/main/java/learn/programming/dao/PersonJdbcDao.java
@@ -1,11 +1,13 @@
 package learn.programming.dao;
 
 import learn.programming.entity.Person;
+import learn.programming.mapper.PersonRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -18,6 +20,10 @@ public class PersonJdbcDao {
         return jdbcTemplate.query("SELECT * FROM PERSON", new BeanPropertyRowMapper<>(Person.class));
     }
 
+    public List<Person> findAllByCustomMapper() {
+        return jdbcTemplate.query("SELECT * FROM PERSON", new PersonRowMapper());
+    }
+
     public Person findById(int id) {
         return jdbcTemplate.queryForObject(
                 "select * from person where id = ?",
@@ -27,6 +33,18 @@ public class PersonJdbcDao {
 
     public int deleteById(int id) {
         return jdbcTemplate.update("delete from person where id = ?", new Object[] {id});
+    }
+
+    public int insert(Person p) {
+        return jdbcTemplate.update(
+                "insert into person (id, name, location, birth_date) values(?, ?, ?, ?)",
+                new Object[] {p.getId(), p.getName(), p.getLocation(), new Timestamp(p.getBirthDate().getTime())});
+    }
+
+    public int update(Person p) {
+        return jdbcTemplate.update(
+                "update person set name = ?, location = ?, birth_date = ? where id = ?",
+                new Object[] {p.getName(), p.getLocation(), new Timestamp(p.getBirthDate().getTime()), p.getId()});
     }
 
 }
